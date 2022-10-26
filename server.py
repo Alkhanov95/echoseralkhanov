3528bd34eaf4ed408079be78576502cff1e5eafd
Sab8rz <115807136+Sab8rz@users.noreply.github.com>
2022-10-26 20:03:44 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,13 @@
 import socket
+import pickle
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
+sock.listen(1)
 conn, addr = sock.accept()
-print(addr)
 
-msg = ''
-
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
+msg = conn.recv(1024)
 print(msg)
+conn.send(msg)
 
 conn.close()
